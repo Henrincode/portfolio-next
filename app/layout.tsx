e76cc5f7f1ef1bf1d4d429289ce6453de1bac363
Henrincode <spellctro@gmail.com>
2025-12-02 14:54:35 -0300

    feat: configurando metatags do html

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,43 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Henrique Marques",
   description: "Dev Fullstack",
+
+  keywords: [
+    "Henrique Marques",
+    "Desenvolvedor Fullstack",
+    "Programador",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node.js",
+    "Supabase"
+  ],
+
+  authors: [
+    { name: "Henrique Marques" }
+  ],
+
+  creator: "Henrique Marques",
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-image-preview": "large",
+      "max-video-preview": -1,
+      "max-snippet": -1
+    }
+  },
+
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1
+  },
 };
 
 export default function RootLayout({
